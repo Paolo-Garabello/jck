e60e1cc74a4d35ee4f8bb861d2594f2aed5c730c
Sebastiano <racca.sebastiano.7805@gmail.com>
2024-02-29 10:57:47 +0100

    Add private chats

---
+++ frontend/src/lib/components/EnableChat.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Chat from './ChatWrapper.svelte'
+
+  export let title: string;
+  export let websocket: WebSocket;
+  export let chat: string;
+
+  let enabled: boolean = false;
+
+  function displayChat() {
+    enabled = !enabled;
+  }
+</script>
+
+
+{#if !enabled}
+  <button on:click={displayChat}>Enable {title}</button>
+{:else}
+  <Chat
+    title={title}
+    websocket={websocket}
+    chat={chat}
+  />
+{/if}
+
+
+
+<style lang="scss">
+  button {
+    padding: 10px;
+    border-radius: 16px;
+    border: 2px solid var(--border);
+    background-color: var(--primary-light);
+    color: var(--secondary-content);
+
+    transition: all .5s;
+
+    &:hover {
+      background-color: var(--primary-dark);
+      color: var(--copy);
+    }
+  }
+</style>
---
+++ frontend/src/routes/dm/[id]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Chat from "$lib/components/ChatWrapper.svelte";
+  import EnableChat from "$lib/components/EnableChat.svelte";
+
+  export let data;
+
+  let websocket: WebSocket = data.websocket;
+</script>
+
+<main>
+
+  <EnableChat
+    title="Public Square"
+    websocket={websocket}
+    chat="public"
+  />
+
+  <Chat
+    title={data.id}
+    websocket={websocket}
+    chat="@{data.id}"
+  />
+</main>
+
+
+<style>
+  main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
